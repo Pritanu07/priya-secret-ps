0288be8614c63fefca6a16346dcd90b9ecea93a9
shanmugapriya <shanmugapriya.rajendran@gmail.com>
2023-08-19 12:58:10 +0800

    secret management

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ module.exports.handler = async (event) => {
       statusCode: 200,
       body: JSON.stringify(
         {
-          message: "Go Serverless v3.0! Your function executed successfully!",
-          input: event,
+          message: "your function executed successfully",
+          access_key: process.env.ACCESS_KEY
         },
         null,
         2
